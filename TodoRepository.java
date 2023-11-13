330887ebfb13b535d86f355635bb34190fce6655
Gaurav Singh <65939916+gsrit@users.noreply.github.com>
2023-11-13 21:35:27 +0530

    Create TodoRepository.java

---
+++ TodoRepository.java
@@ -0,0 +1,14 @@
+package com.yourpackage.repository;
+
+import com.yourpackage.model.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<TodoItem, Long> {
+}
+package com.yourpackage.repository;
+
+import com.yourpackage.model.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<TodoItem, Long> {
+}
